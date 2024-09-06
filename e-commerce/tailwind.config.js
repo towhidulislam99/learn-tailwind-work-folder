45e05f5e84ee645eaec7070964d9696b4f58dff5
towhidulislam99 <towhidulislam618@gmail.com>
2024-09-07 00:13:26 +0600

    Confiq File Change code

--- e-commerce/tailwind.config.js
+++ e-commerce/tailwind.config.js
@@ -7,11 +7,13 @@
 //   plugins: [],
 // }
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.html", "./src/**/*.{js,jsx,ts,tsx}","./css/**/*.{js,jsx,ts,tsx,css}" ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
 
